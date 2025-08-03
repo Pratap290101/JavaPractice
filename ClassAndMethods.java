825c5ad2ead17cb93ec2f37223a96e93112577d8
Pratap2901 <tupepratap91@gmail.com>
2025-08-03 15:49:49 +0530

    practice

---
+++ ClassAndMethods.java
@@ -0,0 +1,5 @@
+class ClassAndMethods {
+    public static void main(String ar[]){
+        System.out.println("hello the charlie");
+    }
+}
--- ClassAndOb.java
+++ ClassAndOb.java
@@ -1,25 +1,5 @@
-import java.util.*;
-class Student{
-        Scanner sc=new Scanner(System.in);
-        String name;
-        int id=0;
-        Student() {
-            getDetails();
-            showDetails();
-        }
-        public void getDetails(){
-            System.out.println("enter your Name:-");
-            name=sc.nextLine();
-            System.out.println("enter your Id:-");
-            id=sc.nextInt();
-        }
-        public void showDetails(){
-            System.out.println("Your name is "+name+" and ID is "+id+" .");
-        }        
-    }
-
-public class ClassAndOb {
-    public static void main(String[] ar){
-        Student s=new Student();
+class ClassAndOb{
+    public static void main(String ar[]){
+        System.out.println("the charlie");
     }
 }
