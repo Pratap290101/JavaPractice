2471f10048dbb7296ffc1018f204adf864d8c902
Pratap2901 <tupepratap91@gmail.com>
2025-08-05 10:35:38 +0530

    classes and objects

--- ClassAndMethods.java
+++ ClassAndMethods.java
@@ -1,5 +1,34 @@
+import java.util.*;
+final class x{
+    Scanner sc=new Scanner(System.in);
+    String name,id;
+    x(){
+        getDetails();
+        printl();
+    }
+    void getDetails(String s){
+        name=s;
+        id="12";
+    }
+    final void getDetails(){
+        System.out.println("enter id and name");
+        id=sc.nextLine();
+        name=sc.next();
+    }
+    void printl(){
+        System.out.println("Student name is "+name+" and Id is "+id+".");
+    }
+}
+
+
 class ClassAndMethods {
     public static void main(String ar[]){
-        System.out.println("hello the charlie");
+        x xx=new x();
+        xx.getDetails("raj");
+        xx.printl();
+        
+        // Instantiate ClassAndMethods to ensure it is used
+        ClassAndMethods cam = new ClassAndMethods();
+        System.out.println(xx.getClass());
     }
 }
--- ClassAndOb.java
+++ ClassAndOb.java
@@ -1,5 +1,28 @@
+class ABC{
+    int x=5;
+    XYZ xy=new XYZ();
+
+    void print(){
+        System.out.println("from class ABC x="+x);
+    }
+    class XYZ{
+        int y=10;
+        void show(){
+            System.out.println("In class XYZ x="+x);
+            System.out.println("In class XYZ y="+y);            
+        }
+        void display(){
+            System.out.println("In class xyz x="+x);
+            System.out.println("In class xyz y="+y);
+        }
+    }
+}
 class ClassAndOb{
     public static void main(String ar[]){
-        System.out.println("the charlie");
+        ABC abc=new ABC();
+        abc.print();
+        abc.xy.show();
+        ABC.XYZ xyz=new ABC().new XYZ();
+        xyz.show();
     }
 }
